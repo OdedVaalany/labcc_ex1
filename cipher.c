43f4ec87e98510ab572989e03a5e090f8f3a7a35
OdedVaalany <avoded2@gmail.com>
2022-03-18 21:12:41 +0200

    cdfd

--- cipher.c
+++ cipher.c
@@ -5,6 +5,11 @@
 /// IN THIS FILE, IMPLEMENT EVERY FUNCTION THAT'S DECLARED IN cipher.h.
 
 #define AB_COUNT 26
+#define CAP_A 65
+#define CAP_Z 90
+#define LIT_A 97
+#define LIT_Z 122
+
 
 /**
  * Calculate the modulo of 26
@@ -19,11 +24,11 @@ int mod26(int x){
 void encode (char s[], int k)
 {
   for(int i=0;i<strlen(s);i++){
-      if(97<=s[i] && s[i]<=122){
-          s[i] = mod26(s[i]-97+k)+97;;
+      if(LIT_A<=s[i] && s[i]<=LIT_Z){
+          s[i] = mod26(s[i]-LIT_A+k)+LIT_A;;
       }
-      if(65<=s[i] && s[i]<=90){
-          s[i] = mod26(s[i]-65+k)+65;
+      if(CAP_A<=s[i] && s[i]<=CAP_Z){
+          s[i] = mod26(s[i]-CAP_A+k)+CAP_A;
       }
   }
 }
@@ -32,11 +37,11 @@ void encode (char s[], int k)
 void decode (char s[], int k)
 {
     for(int i=0;i<strlen(s);i++){
-        if(97<=s[i] && s[i]<=122){
-            s[i] = mod26(s[i]-97-k)+97;
+        if(LIT_A<=s[i] && s[i]<=LIT_Z){
+            s[i] = mod26(s[i]-LIT_A-k)+LIT_A;
         }
-        if(65<=s[i] && s[i]<=90){
-            s[i] = mod26(s[i]-65-k)+65;
+        if(CAP_A<=s[i] && s[i]<=CAP_Z){
+            s[i] = mod26(s[i]-CAP_A-k)+CAP_A;
         }
     }
 }
